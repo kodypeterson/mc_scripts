7d06fc606b83ee60b0cdaf0e9bb1f6754a1b6dcb
Kody Peterson <kody.peterson@disney.com>
2016-06-30 16:50:08 -0400

    updates

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo -e "Starting Minecraft.."
 sleep 5
 cd /srv/minecraft/
 
-screen -A -m -d -S minecraftsvr bash -c "java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -Dcom.mojang.eula.agree=true -jar /srv/minecraft/spigot-1.10.2.jar | tee server.out"
+screen -A -m -d -S minecraftsvr bash -c "java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -Dcom.mojang.eula.agree=true -jar /srv/minecraft/spigot-1.10.2.jar"
